573c435f483ca168a32365e26debb7972c9eb3ba
giorgi gogoladze <gogoladzeg321@gmail.com>
2024-08-05 02:16:01 +0400

    add prettier

--- components/login/header.tsx
+++ components/login/header.tsx
@@ -1,33 +1,35 @@
-import {View, Image, TouchableHighlight} from "react-native";
-import React from "react";
-import {Ionicons} from "@expo/vector-icons";
-import ReText from "@/components/shared/ReText";
-import {useColorScheme} from "nativewind";
+import { View, Image, TouchableHighlight } from 'react-native';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import ReText from '@/components/shared/ReText';
+import { useColorScheme } from 'nativewind';
 
 const LoginHeader = () => {
-  const {colorScheme, toggleColorScheme} = useColorScheme();
+  const { colorScheme, toggleColorScheme } = useColorScheme();
 
   return (
     <View
       className="min-h-[30vh] h-1/3 w-full"
-      style={{opacity: colorScheme === "light" ? 0.6 : 1}}
+      style={{ opacity: colorScheme === 'light' ? 0.6 : 1 }}
     >
       <Image
-        source={require("@/assets/images/login/topimage.png")}
+        source={require('@/assets/images/login/topimage.png')}
         className="h-full absolute top-0"
         resizeMode="stretch"
         blurRadius={100}
       />
       <TouchableHighlight onPress={toggleColorScheme}>
         <View className="absolute flex-row items-center top-20 right-8">
-          <Ionicons name="rose" color="white" size={22} />
+          <Ionicons
+            name="rose"
+            color="white"
+            size={22}
+          />
           <ReText className="font-bold text-white text-2xl">Bank</ReText>
         </View>
       </TouchableHighlight>
       <View className="absolute bottom-0 p-8">
-        <ReText className="text-5xl font-bold text-white">
-          Sign in to your Account
-        </ReText>
+        <ReText className="text-5xl font-bold text-white">Sign in to your Account</ReText>
       </View>
     </View>
   );
